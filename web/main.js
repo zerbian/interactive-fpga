784964c21cad09bea3d1d5c32cb47c504f9885dc
zerbian <zerbian@campus.tu-berlin.de>
2021-07-08 13:14:39 +0200

    remove old comments and add new one

--- web/main.js
+++ web/main.js
@@ -1,33 +1,14 @@
-/*
-function loadJSON(filename, callback) {
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', filename, true);
-    xobj.onreadystatechange = function() {
-        if (xobj.readyState == 4 && xobj.status == "200") callback(xobj.responseText);
-    }
-    xobj.send(null);
-}
-*/
-
-function loadJSON(filename, callback) {
+// fetch a file via the file / path
+function fetchFile(filename, callback) {
     fetch(filename, {method: 'GET'})
         .then(res => res.json())
         .then(json => callback(json))
         .catch(err => console.error(err));
 }
 
-// function loadCircuit(id, filename) {
-//     loadJSON(filename, json => {
-//         //var json = JSON.parse(res);
-//         var cir = new digitaljs.Circuit(json);
-//         var can = cir.displayOn($('#' + id));
-//         cir.start();
-//     });
-// };
-
+// load a digitalJS circuit using a htmlElement and the filename  / path
 function loadCircuit(htmlElement, filename) {
-    loadJSON(filename, json => {
+    fetchFile(filename, json => {
         //var json = JSON.parse(res);
         var cir = new digitaljs.Circuit(json);
         var can = cir.displayOn(htmlElement);
@@ -35,15 +16,7 @@ function loadCircuit(htmlElement, filename) {
     });
 };
 
-// var circuits = [
-//     //{"id":"c1", "path": "./circuits/clb.json"},
-//     {"id":"c2", "path": "./circuits/1bitRegister.json"}
-// ];
-
-// circuits.forEach(value => {
-//     loadCircuit(value.id, value.path);
-// });
-
+// create circuits for all circuits in HTML
 var circuits = document.getElementsByClassName('circuit')
 for (let elem of circuits) {
     var file = "./circuits/" + elem.getAttribute("circuit") + ".json";
